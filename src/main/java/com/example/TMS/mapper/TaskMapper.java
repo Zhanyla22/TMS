cc405592116e4d00b8d2a29ae69fd1c3a3e20dfe
ZHANYLAI MAMYTOVA <ja.mamytova@gmail.com>
2023-12-13 15:04:40 +0600

    added MapStruct

--- src/main/java/com/example/TMS/mapper/TaskMapper.java
+++ src/main/java/com/example/TMS/mapper/TaskMapper.java
@@ -1,32 +1,15 @@
 package com.example.TMS.mapper;
 
+import com.example.TMS.dto.request.AddTaskRequest;
 import com.example.TMS.dto.response.InfoTaskResponse;
-import com.example.TMS.dto.response.TaskDeleteResponse;
 import com.example.TMS.entity.Task;
-import org.springframework.stereotype.Service;
+import com.example.TMS.entity.User;
+import org.mapstruct.Mapper;
 
-@Service
-public class TaskMapper {
+@Mapper(componentModel = "spring")
+public interface TaskMapper {
 
-    public static InfoTaskResponse entityToResponse(Task task) {
-        return InfoTaskResponse.builder()
-                .title(task.getTitle())
-                .description(task.getDescription())
-                .uuid(task.getUuid())
-                .id(task.getId())
-                .priority(task.getPriority())
-                .statusTask(task.getStatusTask())
-                .status(task.getStatus())
-                .author(UsersMapper.entityToResponse(task.getAuthor()))
-                .executor(UsersMapper.entityToResponse(task.getExecutor()))
-                .createdDate(task.getCreatedDate())
-                .build();
-    }
+    InfoTaskResponse toModel(Task task);
 
-    public static TaskDeleteResponse entityToDeleteResponse(Task task){
-        return TaskDeleteResponse.builder()
-                .uuid(task.getUuid())
-                .status(task.getStatus())
-                .build();
-    }
+    Task toEntity(AddTaskRequest addTaskRequest, User author, User executor);
 }
